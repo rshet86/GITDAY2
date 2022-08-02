693529a1bc36bb927b3d6ea85e0e3c7f73a63048
HP-PC <HP-PC@10.0.0.93>
2022-08-01 22:22:30 -0400

    inital push

--- src/DEMO/DEMOTEST.java
+++ src/DEMO/DEMOTEST.java
@@ -7,4 +7,14 @@ public class DEMOTEST {
 
 	}
 
+
+// for us 123..
+public static void add(int a,int b) {
+	System.out.println(a+b);
+}
+
+//for us345
+public static void divide(int a,int b) {
+	System.out.println(a/b);
+}
 }
